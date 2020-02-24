53e1a84c237971f99f80a15340f23a88a44d3e43
Leonardo Coutinho Gomes <lcoutinhog@indracampos.local>
2020-02-23 21:27:00 -0300

    Add first effect at the banner

--- js/banner.js
+++ js/banner.js
@@ -1,18 +1,25 @@
-counter = 1;
+// counter = 1;
 
-carousel = document.getElementsByClassName("li_img");
-total = carousel.length;
+// carousel = document.getElementsByClassName("li_img");
+// total = carousel.length;
 
-setInterval(() => {
-  if (counter >= total) {
-    for (li of carousel) {
-      li.style.left = 0;
-    }
-    counter = 1
-  } else {
-    for (li of carousel) {
-      li.style.left = `${(-20 * counter)}%`;
-    }
-    counter++;
-  }
-}, 1000);
+// setInterval(() => {
+//   if (counter >= total) {
+//     for (li of carousel) {
+//       li.style.left = 0;
+//     }
+//     counter = 1
+//   } else {
+//     for (li of carousel) {
+//       li.style.left = `${(-20 * counter)}%`;
+//     }
+//     counter++;
+//   }
+// }, 1000);
+
+
+const slide = (element) => {
+  setTimeout((element) => {
+    element.style.left = `${(-20 * counter)}%`;
+  }, 1000);
+}
